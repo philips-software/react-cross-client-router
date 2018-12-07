d8bb9db0163256dc701c278c83de605a6b474fe0
Jasper Stam <jasper.stam@philips.com>
2018-12-07 13:20:48 +0100

    Add actual implementation

---
+++ src/Router.test.js
@@ -0,0 +1,122 @@
+import { createMemoryHistory } from 'history';
+import unload from 'unload';
+
+import { Router } from '.';
+
+class MockStorage {
+  constructor() {
+    this.data = {};
+  }
+
+  setItem(key, value) {
+    this.data[key] = value;
+  }
+
+  getItem(key) {
+    return this.data[key];
+  }
+}
+
+class MockChannel {
+  constructor() {
+    this.messages = [];
+  }
+
+  postMessage = msg => {
+    this.onmessage(msg);
+    this.messages.push(msg);
+  };
+}
+
+const TAB_ID = 'henk';
+
+describe('The Router', () => {
+  let channel;
+  let storage;
+  let history;
+  let router;
+  beforeEach(() => {
+    history = createMemoryHistory({
+      initialEntries: [
+        {
+          pathName: '/henk',
+          search: `?tabId=${TAB_ID}&foo=bar`,
+        },
+      ],
+    });
+    channel = new MockChannel();
+    storage = new MockStorage();
+    router = new Router();
+    router.init(history, channel, storage);
+  });
+  it('should find the tabId from the url', () => {
+    expect(router.tabId).toBe(TAB_ID);
+  });
+
+  it('removes only that search param after registering', () => {
+    expect(history.location.search).toBe('?foo=bar');
+  });
+
+  it('should store the tabId in localstorage', () => {
+    expect(storage.getItem('react-cross-tab-router-tabId')).toEqual(TAB_ID);
+  });
+
+  it('should get the tabId from storage when its not found in the url', () => {
+    history = createMemoryHistory({
+      initialEntries: ['/henk'],
+    });
+    storage.setItem('react-cross-tab-router-tabId', 'asdf');
+    router = new Router();
+    router.init(history, channel, storage);
+
+    expect(router.tabId).toBe('asdf');
+  });
+
+  it('should send a JOIN signal', () => {
+    expect(channel.messages).toContainEqual(JSON.stringify(['JOIN', TAB_ID]));
+  });
+
+  it('when leaving should send a LEAVE signal', () => {
+    unload.runAll();
+    expect(channel.messages).toContainEqual(JSON.stringify(['LEAVE', TAB_ID]));
+  });
+
+  it('should add a tabId to memory when encountering a JOIN', () => {
+    channel.postMessage(JSON.stringify(['JOIN', 'newTab']));
+    expect(router.tabs).toContain('newTab');
+  });
+
+  it('remove a tabId from memory when encountering a LEAVE signal', () => {
+    channel.postMessage(JSON.stringify(['JOIN', 'newTab']));
+    channel.postMessage(JSON.stringify(['LEAVE', 'newTab']));
+    expect(router.tabs).not.toContain('newTab');
+  });
+
+  it('should redirect to a page when a OPEN signal is target to this tab', () => {
+    channel.postMessage(
+      JSON.stringify([
+        'OPEN',
+        {
+          targetTab: TAB_ID,
+          location: '/location',
+        },
+      ]),
+    );
+
+    expect(history.location.pathname).toBe('/location');
+  });
+
+  it('should respond with its own tabId (ACK_JOIN) when encountering a JOIN signal', () => {
+    channel.postMessage(JSON.stringify(['JOIN', 'newTab']));
+    expect(channel.messages).toContainEqual(JSON.stringify(['ACK_JOIN', TAB_ID]));
+  });
+
+  it('should not ACK_JOIN its own join', () => {
+    expect(channel.messages).not.toContainEqual(JSON.stringify(['ACK_JOIN', TAB_ID]));
+  });
+
+  it('should add a tabId to memory when encountering an ACK_JOIN', () => {
+    channel.postMessage(JSON.stringify(['ACK_JOIN', 'earlierOpenedTab']));
+    expect(router.tabs).toContain('earlierOpenedTab');
+  });
+});
--- src/index.js
+++ src/index.js
@@ -1,22 +1,189 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import unload from 'unload';
 
-import styles from './styles.css'
+const MASTER_NAME = 'parent';
+const PROTO_JOIN = 'JOIN';
+const PROTO_ACK_JOIN = 'ACK_JOIN';
+const PROTO_LEAVE = 'LEAVE';
+const PROTO_OPEN = 'OPEN';
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
+const URL_PARAM = 'tabId';
+const LOCAL_STORAGE_KEY = 'react-cross-tab-router-tabId';
+
+export class Router {
+  constructor() {
+    this.initialized = false;
+    this.tabs = [];
   }
 
-  render() {
-    const {
-      text
-    } = this.props
+  init(history, channel, storage) {
+    this.history = history;
+    this.channel = channel;
+    this.storage = storage;
+    this.channel.onmessage = this.handleMessage.bind(this);
 
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
+    this.register();
+    unload.add(this.unregister.bind(this));
+    this.initialized = true;
+  }
+
+  handleMessage(rawMessage) {
+    let message;
+    try {
+      message = JSON.parse(rawMessage);
+    } catch (e) {
+      message = rawMessage;
+    }
+
+    const [type, body] = message;
+
+    switch (type) {
+      case PROTO_JOIN: {
+        const newRoomName = body;
+        if (!this.tabs.includes(newRoomName)) {
+          this.tabs.push(newRoomName);
+        }
+
+        if (newRoomName !== this.tabId) {
+          this.acknowledgeJoin();
+        }
+        break;
+      }
+      case PROTO_ACK_JOIN: {
+        const newRoomName = body;
+        if (!this.tabs.includes(newRoomName)) {
+          this.tabs.push(newRoomName);
+        }
+        break;
+      }
+
+      case PROTO_LEAVE: {
+        const targetRoomName = body;
+        const tabIndex = this.tabs.indexOf(targetRoomName);
+        this.tabs.splice(tabIndex, 1);
+        break;
+      }
+
+      case PROTO_OPEN:
+        if (body.targetTab !== this.tabId) {
+          break;
+        }
+        this.history.push(body.location);
+        break;
+
+      default: {
+        console.error('Unknown message type', type); /* eslint-disable-line */
+      }
+    }
+  }
+
+  acknowledgeJoin() {
+    this.sendMessage([PROTO_ACK_JOIN, this.tabId]);
+  }
+
+  register() {
+    // Find out what the name of this tab is
+    const { location } = this.history;
+    const searchParams = new URLSearchParams(location.search);
+    let tabId = searchParams.get(URL_PARAM);
+
+    if (tabId) {
+      searchParams.delete(URL_PARAM);
+
+      // A bit nasty that we have to compile the url on our own
+      // If we push to url.href, it will will append the full url as relative path.
+      this.history.push(`${location.pathname}?${searchParams}`);
+    } else {
+      tabId = this.storage.getItem(LOCAL_STORAGE_KEY) || MASTER_NAME;
+    }
+
+    this.tabId = tabId;
+    this.tabs.push(tabId);
+
+    this.storage.setItem(LOCAL_STORAGE_KEY, tabId);
+    this.sendMessage([PROTO_JOIN, tabId]);
+  }
+
+  unregister() {
+    this.sendMessage([PROTO_LEAVE, this.tabId]);
+  }
+
+  sendMessage(message) {
+    if (typeof message !== 'object') {
+      throw new Error('Only JSON can be sent over the broadcast channel');
+    }
+    this.channel.postMessage(JSON.stringify(message));
+  }
+
+  redirectTargetTab(targetTab, location) {
+    this.sendMessage([
+      PROTO_OPEN,
+      {
+        targetTab,
+        location,
+      },
+    ]);
   }
 }
+
+export const router = new Router();
+
+const CrossTabRouter = ({ history, channel, storage }) => {
+  if (!router.initialized) {
+    router.init(history, channel, storage);
+  }
+
+  return null;
+};
+
+export class CrossTabLink extends Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    to: PropTypes.string.isRequired,
+    targetTab: PropTypes.string,
+  };
+
+  static defaultProps = {
+    targetTab: 'child',
+  };
+
+  handleClick = e => {
+    const { targetTab, to } = this.props;
+
+    if (!router.tabs.includes(targetTab)) {
+      return;
+    }
+
+    // if that target already exists, don't open a new tab
+    e.preventDefault();
+
+    if (targetTab === router.tabId) {
+      router.history.push(to);
+      return;
+    }
+
+    router.redirectTargetTab(targetTab, to);
+  };
+
+  render() {
+    const { children, to, targetTab } = this.props;
+
+    const scopedHref = `${to}?tabId=${targetTab}`;
+    return (
+      <a href={scopedHref} target="_blank" rel="noopener noreferrer" onClick={this.handleClick}>
+        {children}
+      </a>
+    );
+  }
+}
+
+CrossTabRouter.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+    location: PropTypes.shape({}).isRequired,
+  }).isRequired,
+};
+
+export default withRouter(CrossTabRouter);

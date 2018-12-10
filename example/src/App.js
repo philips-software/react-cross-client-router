d0961b89ba392474d87baffec8e82c917db02c92
Jasper Stam <jasper.stam@philips.com>
2018-12-10 16:43:37 +0100

    Add example app

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom';
+import { CrossRouterProvider } from 'react-cross-client-router'
 
-import ExampleComponent from 'react-cross-client-router'
+const ListView = () => (
+  <ul>
+    <li>Item 1</li>
+    <li>Item 2</li>
+    <li>Item 3</li>
+    <li>Item 4</li>
+  </ul>
+);
+
+const DetailView = () => (
+  <div>Details for item X</div>
+);
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <p>EXAMPL E </p>
+        <Route path="/" exact component={ListView} />
+        <Route path="/detail" component={DetailView} />
       </div>
     )
   }
--- example/src/index.js
+++ example/src/index.js
@@ -1,7 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import BroadcastChannel from 'broadcast-channel';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { CrossRouterProvider } from 'react-cross-client-router'
 
-import './index.css'
-import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <CrossRouterProvider
+      channel={new BroadcastChannel("react-cross-tab-router")}
+      storage={window.sessionStorage}
+    >
+      <App />
+    </CrossRouterProvider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);

24e46a380b9fecde696a9ee87fee8748f69dfd08
Jasper Stam <jasper.stam@philips.com>
2019-01-04 15:37:25 +0100

    Fix public_url and add it to the react-router basename

--- example/src/index.js
+++ example/src/index.js
@@ -4,12 +4,17 @@ import BroadcastChannel from 'broadcast-channel';
 import { BrowserRouter } from 'react-router-dom';
 import { ClientRouterProvider } from 'react-cross-client-router'
 
-
 import './index.css';
 import App from './App';
 
+let relativePath = '';
+if (process.env.PUBLIC_URL) {
+  const publicUrl = new URL(process.env.PUBLIC_URL);
+  relativePath = publicUrl.pathname;
+}
+
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={relativePath}>
     <ClientRouterProvider
       channel={new BroadcastChannel('react-cross-tab-router')}
       storage={window.sessionStorage}
